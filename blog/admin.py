1dc518fd813054e50121b9197163d3cf34301c97
Dante Omar Arciga Camarena <dante.omar.arciga@gmail.com>
2013-10-05 01:06:58 -0500

    Se agregaron los archivos de la aplicacion
    Aun no funciona bien

---
+++ blog/admin.py
@@ -0,0 +1,5 @@
+from blog.models import Blog, Autor, Entrada
+from django.contrib import admin
+admin.site.register(Blog)
+admin.site.register(Autor)
+admin.site.register(Entrada)
---
+++ blog/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+# Create your models here.
+class Blog(models.Model):
+	nombre = models.CharField(max_length=100)
+	etiqueta = models.TextField()
+
+	def __unicode__(self):
+		return self.nombre
+class Autor(models.Model):
+	nombre = models.CharField(max_length=50)
+	email = models.EmailField()
+
+	def __unicode__(self):
+		return self.nombre
+class Entrada(models.Model):
+	"""docstring for Entrada"""
+	blog = models.ForeignKey(Blog)
+	titulo = models.CharField(max_length=255)
+	cuerpo_texto = models.TextField()
+	fecha_publicacion = models.DateField()
+	mod_date = models.DateField()
+	autor = models.ManyToManyField(Autor)
+	n_comentarios = models.IntegerField()
+
+	def __init__(self):
+		return self.titulo
+		
+		
